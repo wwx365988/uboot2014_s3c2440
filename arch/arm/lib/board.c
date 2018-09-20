6d8697cc8eec06e29a6bdbae556a16a59334d359
wwx365988 <17761217301@163.com>
2018-09-21 01:35:09 +0800

    all ok............

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -654,6 +654,9 @@ void board_init_r(gd_t *id, ulong dest_addr)
 	}
 #endif
 
+	run_command("mtdparts default", 0);
+	//mtdparts_init();	
+
 	/* main_loop() can return to retry autoboot, if so just run it again. */
 	for (;;) {
 		main_loop();
